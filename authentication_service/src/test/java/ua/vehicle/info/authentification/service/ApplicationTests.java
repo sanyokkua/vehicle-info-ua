abb0f4fc43fc27d110cfbd4a1f9dae8b89a4e7a3
Alexander Kostenko <sanyokkua@gmail.com>
2020-05-02 19:31:46 +0300

    Added stub spring-boot apps (services). Configured parent project pom.

---
+++ authentication_service/src/test/java/ua/vehicle/info/authentification/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vehicle.info.authentification.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
